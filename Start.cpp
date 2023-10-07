d7aa21d99812a6c7cc4b11cfaf0c3482d71ba9a7
xbh <binghexiao@163.com>
2023-10-07 10:17:59 +0800

    完整数据  效率提高7倍

--- Start.cpp
+++ Start.cpp
@@ -131,15 +131,15 @@ void MultiObjectVsSingleObject() {
     vector<H3_D> Comprehensive(17, H3_D());
     vector<H3_P> PointMap(17, H3_P());
     //用于统计参数数据
-    ofstream log("D:/桌面/log1001.txt",ios::app);
+    ofstream log("D:/桌面/log1006.txt",ios::app);
     //基准格网层级
     int level = 10;
     //多层次格网层级
     int target_level = 16;
     //基准格网数据路径
     // string filepath = "C:\\Users\\69025\\Desktop\\路径规划\\论文实验\\英文论文实验\\改进算法后实验结果\\实验数据_10.csv";
-    //string filepath = "D:/桌面/路径规划/已有资料/改进算法后实验结果/实验数据_10.csv";
-    string filepath = "D:/桌面/966数据_10.csv";
+    string filepath = "D:/桌面/路径规划/已有资料/改进算法后实验结果/实验数据_10.csv";
+    //string filepath = "D:/桌面/966数据_10.csv";
     //string filepath = "D:/桌面/小数据.csv";
     // string m_filepath = "C:\\Users\\69025\\Desktop\\路径规划\\论文实验\\英文论文实验\\改进算法后实验结果\\other_compact_h3_res=10_算法用.csv";
     //string m_filepath = "D:\\桌面\\改进算法后实验结果\\other_compact_h3_res=10_算法用.csv";
@@ -148,7 +148,7 @@ void MultiObjectVsSingleObject() {
     // 读取基准格网数据
     Util::loadingData(PointMap, DEM, Comprehensive, Factor, level, filepath);   
     // 读取多层次格网数据，并生成多层次格网地图
-    //Util::MultiObjectMultiHierarchy(PointMap, DEM, Comprehensive, Factor, level, m_filepath);
+    Util::MultiObjectMultiHierarchy(PointMap, DEM, Comprehensive, Factor, level, m_filepath);
     // 多粒度模型
     // 超小数据的起始点
     /*H3Index h3_start = 0x8a4008b9d507fff;
@@ -156,13 +156,13 @@ void MultiObjectVsSingleObject() {
     /*H3Index h3_end = 0x8a4008b9d49ffff;*/
     
      //部分数据的开始点和结束点
-    H3Index h3_start = 0x8a4008b9d06ffff;
-    H3Index h3_end = 0x8a4008b9eadffff;
+    /*H3Index h3_start = 0x8a4008b9d06ffff;
+    H3Index h3_end = 0x8a4008b9eadffff;*/
 
 
     //// 完整数据的起点和终点
-    /*H3Index h3_start = 0x8a400889c157fff;
-    H3Index h3_end = 0x8a400881b487fff;*/
+    H3Index h3_start = 0x8a400889c157fff;
+    H3Index h3_end = 0x8a400881b487fff;
     
     vector<pair<vector<H3Index>, point_type>> result;
     cout << "寻路启动..." << endl;
@@ -182,69 +182,18 @@ void MultiObjectVsSingleObject() {
     vector<double> mean;//坡度均值
     vector<double> dev;//坡度标准差
     vector<vector<double>> factor;
-    //for (int i = 0; i < Turn; i++) {
-    //    double time;
-    //    result = Util::MultiObjectMultiHierarchySearch(PointMap, DEM, Comprehensive, target_level, level, h3_start, h3_end, log, Max_of_the_Number_of_Open_Grid, Traverses_the_Number_of_Grid, time);
-    //    if (i == 0) {
-    //        Util::AnalysisData(PointMap[target_level], DEM[target_level], Comprehensive[target_level], result, Length_of_Path);
-    //       Util::ComputeFactor(result, Factor[target_level], factor);
-    //        Util::ComputeGrad(result, DEM[target_level], mean, dev);
-    //    }
-    //    totaltime += time;
-    //}
-    //if (result.size() == 0)
-    //    cout << "未找到路径" << endl;
-    //log << "时间：" << totaltime / Turn << "  Open队列最大深度：" << Max_of_the_Number_of_Open_Grid / Turn << " 格网遍历数：" << Traverses_the_Number_of_Grid / Turn << ";" << endl;
-    //cout << "时间：" << totaltime / Turn << "  Open队列最大深度：" << Max_of_the_Number_of_Open_Grid / Turn << " 格网遍历数：" << Traverses_the_Number_of_Grid / Turn << ";" << endl;
-    //// 分路径
-    //for (int i = 0; i < result.size(); i++) {
-    //    log << "Path {" << i << "} :";
-    //    cout << "Path {" << i << "} :";
-    //    log << "路径格网数：" << result[i].first.size() << "; 路径长度：" << Length_of_Path[i] << "; 因子：";
-    //    cout << "路径格网数：" << result[i].first.size() << "; 路径长度：" << Length_of_Path[i] << "; 因子：";
-    //    // 因子
-    //    for (int j = 0; j < factor[i].size(); j++)
-    //    {
-    //        log << factor[i][j] * 100 << " ";
-    //        cout << factor[i][j] * 100 << " ";
-    //    }
-    //        
-    //    // 坡度均值与标准差
-    //    log << "; 均值：" << mean[i] << "; 标准差：" << dev[i] << endl;
-    //    cout << "; 均值：" << mean[i] << "; 标准差：" << dev[i] << endl;
-    //}
-    ////ofstream outfile("D:\\桌面\\改进算法后实验结果\\多粒度.geojson");
-    //ofstream outfile("D:/桌面/多粒度.geojson");
-    //outfile << Util::getGeoJson(result, "多粒度");
-    //outfile.close();
-    cout << "普通：" << endl;
-    auto time = Util::PrintCurrentTime();
-    log << time << endl;
-    log << "普通：" << endl;
-  //  //totaltime = 0;
-  ///*  Max_of_the_Number_of_Open_Grid = 0;
-  //  Traverses_the_Number_of_Grid = 0;
-  //  Length_of_Path = vector<double>();
-  //  mean = vector<double>();
-  //  dev = vector<double>();
-  //  factor = vector<vector<double>>();*/
     for (int i = 0; i < Turn; i++) {
         double time;
-        //result = Util::MultiObjectMultiHierarchySearch(PointMap, DEM, Comprehensive, level, level, h3_start, h3_end, log, Max_of_the_Number_of_Open_Grid, Traverses_the_Number_of_Grid, time);
-        result = Util::MultiObjectMultiHierarchySearch(PointMap, DEM, Comprehensive, level, level, h3_start, h3_end, log, Max_of_the_Number_of_Open_Grid, Traverses_the_Number_of_Grid, time);
+        result = Util::MultiObjectMultiHierarchySearch(PointMap, DEM, Comprehensive, target_level, level, h3_start, h3_end, log, Max_of_the_Number_of_Open_Grid, Traverses_the_Number_of_Grid, time);
         if (i == 0) {
-            //Util::AnalysisData(PointMap[target_level], DEM[target_level], Comprehensive[target_level], result, Length_of_Path);
-            Util::AnalysisData(PointMap[level], DEM[level], Comprehensive[level], result, Length_of_Path);
-            cout << "analysisData运行完毕" << endl;
-            Util::ComputeFactor(result, Factor[level], factor);
-            Util::ComputeGrad(result, DEM[level], mean, dev);
+            Util::AnalysisData(PointMap[target_level], DEM[target_level], Comprehensive[target_level], result, Length_of_Path);
+           Util::ComputeFactor(result, Factor[target_level], factor);
+            Util::ComputeGrad(result, DEM[target_level], mean, dev);
         }
         totaltime += time;
     }
-    if (result.size() == 0) {
+    if (result.size() == 0)
         cout << "未找到路径" << endl;
-        return;
-    }
     log << "时间：" << totaltime / Turn << "  Open队列最大深度：" << Max_of_the_Number_of_Open_Grid / Turn << " 格网遍历数：" << Traverses_the_Number_of_Grid / Turn << ";" << endl;
     cout << "时间：" << totaltime / Turn << "  Open队列最大深度：" << Max_of_the_Number_of_Open_Grid / Turn << " 格网遍历数：" << Traverses_the_Number_of_Grid / Turn << ";" << endl;
     // 分路径
@@ -263,12 +212,63 @@ void MultiObjectVsSingleObject() {
         // 坡度均值与标准差
         log << "; 均值：" << mean[i] << "; 标准差：" << dev[i] << endl;
         cout << "; 均值：" << mean[i] << "; 标准差：" << dev[i] << endl;
-        
     }
-    ofstream outfile1("D:/桌面/普通.geojson");
-    //ofstream outfile1("D:\\桌面\\改进算法后实验结果\\普通.geojson");
-    outfile1 << Util::getGeoJson(result, "普通");
-    outfile1.close();
+    //ofstream outfile("D:\\桌面\\改进算法后实验结果\\多粒度.geojson");
+    ofstream outfile("D:/桌面/多粒度.geojson");
+    outfile << Util::getGeoJson(result, "多粒度");
+    outfile.close();
+  //  cout << "普通：" << endl;
+  //  auto time = Util::PrintCurrentTime();
+  //  log << time << endl;
+  //  log << "普通：" << endl;
+  ////  //totaltime = 0;
+  /////*  Max_of_the_Number_of_Open_Grid = 0;
+  ////  Traverses_the_Number_of_Grid = 0;
+  ////  Length_of_Path = vector<double>();
+  ////  mean = vector<double>();
+  ////  dev = vector<double>();
+  ////  factor = vector<vector<double>>();*/
+  //  for (int i = 0; i < Turn; i++) {
+  //      double time;
+  //      //result = Util::MultiObjectMultiHierarchySearch(PointMap, DEM, Comprehensive, level, level, h3_start, h3_end, log, Max_of_the_Number_of_Open_Grid, Traverses_the_Number_of_Grid, time);
+  //      result = Util::MultiObjectMultiHierarchySearch(PointMap, DEM, Comprehensive, level, level, h3_start, h3_end, log, Max_of_the_Number_of_Open_Grid, Traverses_the_Number_of_Grid, time);
+  //      if (i == 0) {
+  //          //Util::AnalysisData(PointMap[target_level], DEM[target_level], Comprehensive[target_level], result, Length_of_Path);
+  //          Util::AnalysisData(PointMap[level], DEM[level], Comprehensive[level], result, Length_of_Path);
+  //          cout << "analysisData运行完毕" << endl;
+  //          Util::ComputeFactor(result, Factor[level], factor);
+  //          Util::ComputeGrad(result, DEM[level], mean, dev);
+  //      }
+  //      totaltime += time;
+  //  }
+  //  if (result.size() == 0) {
+  //      cout << "未找到路径" << endl;
+  //      return;
+  //  }
+  //  log << "时间：" << totaltime / Turn << "  Open队列最大深度：" << Max_of_the_Number_of_Open_Grid / Turn << " 格网遍历数：" << Traverses_the_Number_of_Grid / Turn << ";" << endl;
+  //  cout << "时间：" << totaltime / Turn << "  Open队列最大深度：" << Max_of_the_Number_of_Open_Grid / Turn << " 格网遍历数：" << Traverses_the_Number_of_Grid / Turn << ";" << endl;
+  //  // 分路径
+  //  for (int i = 0; i < result.size(); i++) {
+  //      log << "Path {" << i << "} :";
+  //      cout << "Path {" << i << "} :";
+  //      log << "路径格网数：" << result[i].first.size() << "; 路径长度：" << Length_of_Path[i] << "; 因子：";
+  //      cout << "路径格网数：" << result[i].first.size() << "; 路径长度：" << Length_of_Path[i] << "; 因子：";
+  //      // 因子
+  //      for (int j = 0; j < factor[i].size(); j++)
+  //      {
+  //          log << factor[i][j] * 100 << " ";
+  //          cout << factor[i][j] * 100 << " ";
+  //      }
+  //          
+  //      // 坡度均值与标准差
+  //      log << "; 均值：" << mean[i] << "; 标准差：" << dev[i] << endl;
+  //      cout << "; 均值：" << mean[i] << "; 标准差：" << dev[i] << endl;
+  //      
+  //  }
+  //  ofstream outfile1("D:/桌面/普通.geojson");
+  //  //ofstream outfile1("D:\\桌面\\改进算法后实验结果\\普通.geojson");
+  //  outfile1 << Util::getGeoJson(result, "普通");
+  //  outfile1.close();
     log.close();
     //log_size.close();
 
